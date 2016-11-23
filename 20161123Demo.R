721eaca231350f4328f95fd2fd142f422e23c31a
David Chiu <tr.ywchiu@gmail.com>
2016-11-23 15:49:27 +0800

    add caret

--- 20161123Demo.R
+++ 20161123Demo.R
@@ -50,3 +50,41 @@ table(ifelse(predicted > 0.5, 'virginica', 'versicolor'), iris[,5])
 fit2        <- ctree(Species~.,data=iris)
 predicted2 <- predict(fit2, iris[,1:4], type= "response")
 table(predicted2, iris[,5])
+
+
+
+library(e1071)
+data(iris)
+fit <-svm(Species~.,iris)
+summary(fit)
+table(predict(fit, iris[,-5]), iris[,5])
+
+
+## SVM with lower cost
+iris.subset = subset(iris, select=c("Sepal.Length", "Sepal.Width", "Species"), Species %in% c("setosa","virginica"))
+plot(x=iris.subset$Sepal.Length,y=iris.subset$Sepal.Width, col=iris.subset$Species, pch=19)
+svm.model = svm(Species ~ ., data=iris.subset, kernel='linear', cost=1, scale=FALSE)
+points(iris.subset[svm.model$index,c(1,2)],col="blue",cex=2)
+w = t(svm.model$coefs) %*% svm.model$SV
+b = -svm.model$rho
+abline(a=-b/w[1,2], b=-w[1,1]/w[1,2], col="red", lty=5)
+plot(x=iris.subset$Sepal.Length,y=iris.subset$Sepal.Width, col=iris.subset$Species, pch=19)
+
+## SVM with higher cost
+svm.model <- svm(Species ~ ., data=iris.subset, type='C-classification', kernel='linear', cost=10000, scale=FALSE)
+points(iris.subset[svm.model$index,c(1,2)],col="blue",cex=2)
+w <- t(svm.model$coefs) %*% svm.model$SV
+b <- -svm.model$rho
+abline(a=-b/w[1,2], b=-w[1,1]/w[1,2], col="red", lty=5)
+
+
+## http://scikit-learn.org/stable/_images/sphx_glr_plot_iris_0012.png
+
+
+## 使用caret 呼叫不同的機器學習函式
+library(caret)
+
+rpartfit <- train(Species ~.  , data = iris, method = "rpart")
+rffit    <- train(Species ~.  , data = iris, method = "rf")
+svmfit   <- train(Species ~.  , data = iris, method = "svmLinear")
+names(getModelInfo())
