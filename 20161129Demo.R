be73c4d401b219a2846cdb2c456e43ef70d608f4
David Chiu <tr.ywchiu@gmail.com>
2016-11-29 16:19:19 +0800

    ROC

--- 20161129Demo.R
+++ 20161129Demo.R
@@ -44,3 +44,90 @@ table(testset$churn, predictions)
 
 (95 + 863) / 1018
 
+
+library(caret)
+cm <- confusionMatrix(table(testset$churn, predictions))
+cm$overall[1]
+
+## DIY cross validation　
+res <- data.frame()
+set.seed(2)
+ind <- sample(10, nrow(churnTrain), replace=TRUE)
+for (i in seq(1,10)){
+  trainset <- churnTrain[ind != i, ]
+  testset  <- churnTrain[ind == i, ]
+  fit <- rpart(churn ~., trainset)
+  predictions <- predict(fit, testset, type= 'class')
+  cm <- confusionMatrix(table(testset$churn, predictions))
+  res <- rbind(res, cm$overall)
+}
+
+## Use caret 進行K-fold cross-validation
+library(caret)
+control <- trainControl(method="repeatedcv", number=10, repeats=3)
+model   <- train(churn~., data=trainset, method="rpart", preProcess="scale", trControl=control)
+model
+
+## 找出最重要的變數
+library(rminer)
+model=fit(churn~.,trainset,model="svm")
+VariableImportance=Importance(model,trainset,method="sensv")
+L=list(runs=1,sen=t(VariableImportance$imp),sresponses=VariableImportance$sresponses)
+mgraph(L,graph="IMP",leg=names(trainset),col="gray",Grid=10)
+
+
+## 尋找高相關性變數
+new_train <- trainset[,!names(churnTrain)%in%c("churn", "international_plan", "voice_mail_plan")]
+cor_mat   <- cor(new_train)
+highlyCorrelated <- findCorrelation(cor_mat, cutoff=0.75)
+names(new_train)[highlyCorrelated]
+
+
+## ROC
+
+set.seed(2)
+ind      <-sample(2, nrow(churnTrain), replace=TRUE, prob=c(0.7, 0.3))
+trainset <- churnTrain[ind==1,]
+testset  <- churnTrain[ind==2,]
+
+library(rpart)
+churn.rp<-rpart(churn ~., data=trainset)
+predictions <- predict(prune.tree, testset)
+
+
+pred_result <- as.factor(ifelse(predictions[,1] > 0.2, 0, 1))
+levels(pred_result) <- c('yes', 'no')
+
+tb <- table(testset$churn, pred_result)
+cm <- confusionMatrix(tb)
+sens <- cm$byClass[1]
+spec <- cm$byClass[2]
+
+# DIY ROC Curve
+res <- data.frame()
+for (cost in seq(0,1,0.1)){
+  pred_result <- as.factor(ifelse(predictions[,1] >= cost, 0, 1))
+  levels(pred_result) <- c('yes', 'no')
+  
+  tb <- table(testset$churn, pred_result)
+  cm <- confusionMatrix(tb)
+  sens <- cm$byClass[1]
+  spec <- cm$byClass[2]
+  res <- rbind(res, data.frame('TPR' = sens, 'FPR' = 1 - spec)) 
+}
+
+res[is.na(res[,2]), 2] <- 0
+res <- rbind(res, data.frame('TPR' = 1, 'FPR' = 1))
+
+plot(TPR ~ FPR, data = res, type= 'l', col='red', xlim=c(0,1))
+points(TPR ~ FPR, data = res, col='blue')
+
+
+## Use ROCR
+library(ROCR)
+predictions  <- predict(churn.rp, testset, type="prob")
+pred.to.roc  <- predictions[, 1]
+pred.rocr    <- prediction(pred.to.roc, as.factor(testset$churn))
+perf.rocr    <- performance(pred.rocr, measure ="auc", x.measure="cutoff")
+perf.tpr.rocr<-performance(pred.rocr, "tpr","fpr")
+plot(perf.tpr.rocr, colorize=T,main=paste("AUC:",(perf.rocr@y.values)))
